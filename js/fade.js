37218bdeabfd555fa1c45e097930ef88132df248
Justin Felcan <felcans@gmail.com>
2015-11-09 17:30:34 -0500

    ie fix
    ie fixes - grab position of scroll

--- js/fade.js
+++ js/fade.js
@@ -1,10 +1,14 @@
-var backdrop = document.getElementById('backdrop');
-var scroll = document.getElementById('scroll')
-document.body.style.minHeight = window.innerHeight * 2 + 'px';
-window.addEventListener('scroll', function(ev) {
-    var target = 1 - ((document.documentElement.scrollTop || window.scrollY) / (window.innerHeight * 0.5));
-    if (target < 0.1) target = 0.1;
-    else if (target > 1) target = 1;
-   backdrop.style.opacity = target;
-   scroll.style.opacity = target;
-});
+(function() {
+	var backdrop = document.getElementById('backdrop');
+	var scroll = document.getElementById('scroll')
+	document.body.style.minHeight = window.innerHeight * 2 + 'px';
+	function position(ev) {
+	    var target = 1 - ((document.documentElement.scrollTop || window.scrollY) / (window.innerHeight * 0.5));
+	    if (target < 0.1) target = 0.1;
+	    else if (target > 1) target = 1;
+	   backdrop.style.opacity = target;
+	   scroll.style.opacity = target;
+	}
+	window.addEventListener('scroll', position);
+	position();
+})();
