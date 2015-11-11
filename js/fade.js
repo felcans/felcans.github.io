5eaeb1404b7d58d38a570ea65596e31a26185b94
Justin Felcan <felcans@gmail.com>
2015-11-11 13:10:43 -0500

    header fade
    fade header on scroll

--- js/fade.js
+++ js/fade.js
@@ -1,13 +1,13 @@
 (function() {
 	var backdrop = document.getElementById('backdrop');
-	var scroll = document.getElementById('scroll')
+	var header = document.getElementById('header')
 	document.body.style.minHeight = window.innerHeight * 2 + 'px';
 	function position(ev) {
 	    var target = 1 - ((document.documentElement.scrollTop || window.scrollY) / (window.innerHeight * 0.5));
 	    if (target < 0.1) target = 0.1;
 	    else if (target > 1) target = 1;
 	   backdrop.style.opacity = target;
-	   scroll.style.opacity = target;
+	   header.style.opacity = target;
 	}
 	window.addEventListener('scroll', position);
 	position();
